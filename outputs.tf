8a8d0c2e9a483343e27162719cdd05c78d292147
John Gruber <john.t.gruber@gmail.com>
2026-04-24 14:40:40 -0500

    removed cluster output from cert-manager outputs

--- outputs.tf
+++ outputs.tf
@@ -4,25 +4,6 @@
 # ============================================================
 
 # ============================================================
-# Cluster Info (from data source lookup)
-# ============================================================
-
-output "cluster_id" {
-  description = "ID of the target OpenShift cluster"
-  value       = data.ibm_container_vpc_cluster.cluster.id
-}
-
-output "cluster_name" {
-  description = "Name of the target OpenShift cluster"
-  value       = data.ibm_container_vpc_cluster.cluster.name
-}
-
-output "cluster_crn" {
-  description = "CRN of the target OpenShift cluster"
-  value       = data.ibm_container_vpc_cluster.cluster.crn
-}
-
-# ============================================================
 # cert-manager Outputs
 # ============================================================
 
